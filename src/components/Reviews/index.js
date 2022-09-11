d3ebb8850eb66e14d379d5f6b6b583202f3efcd6
Manuel <30572287+manuel-rw@users.noreply.github.com>
2022-09-11 16:10:24 +0000

    💄 new landing page design
    * 💄improve landing page design
    * 💄 add snow animation on landing page
    * 📱 improve slider, responsiveness and add themed compressed device preview

---
+++ src/components/Reviews/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+export default function GenericReviewContainer({ children }) {
+  return <div className={styles.genericReviewContainer}>{children}</div>;
+}
+
+export const SocialMediaReview = ({ username, profilePicture, link, plattform, fullText }) => {
+  return (
+    <GenericReviewContainer>
+      <div className={styles.genericReviewUserContainer}>
+        <img src={profilePicture} alt="Profile Picture" className={styles.genericReviewUserImage} />
+        <div>
+          <b className={styles.genericReviewUserName}>{username}</b>
+          <a href={link} target="_blank">
+            on {plattform}
+          </a>
+        </div>
+      </div>
+
+      <p>{fullText}</p>
+    </GenericReviewContainer>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,76 @@
 import React from 'react';
-import clsx from 'clsx';
+
 import Link from '@docusaurus/Link';
+import { useColorMode } from '@docusaurus/theme-common';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { IconDownload } from '@tabler/icons';
 import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import { useCallback } from 'react';
 
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageShowcase from '@site/src/components/HomepageShowcase';
+import HomepageUserReviews from '../components/HomepageUserReviews';
 
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import StyledDivider from '../components/Divider/StyledDivider';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <img src="/img/favicon.png" height={200} style={{ filter: 'brightness(500%)' }} />
-        <h1 className="hero__title">Homarr 🦞</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', margin: -5 }}>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/introduction/installation"
-            >
-              Install Homarr 🚀
-            </Link>
+    <header className={clsx('hero hero--primary', styles.heroBanner, styles.hero)}>
+      <div className="container" style={{ zIndex: 1 }}>
+        <div className="row">
+          <div className="col">
+            <h1>A modern and leightweight homepage for your server</h1>
+            <p>
+              Homarr is a simple and modern homepage for your server, that helps you access all of
+              your services in one place. It integrates with the services you use to display useful
+              information or control them. It's easy to install and supports many different devices.
+            </p>
+
+            <div className={styles.heroButtons}>
+              <Link
+                className={clsx('button button--secondary button--lg', styles.heroButton)}
+                to="#features"
+              >
+                Learn more about Homarr
+              </Link>
+              <Link
+                className={clsx('button button--secondary button--lg', styles.heroButton)}
+                to="/docs/introduction/installation"
+              >
+                <span>Install</span>
+                <IconDownload size={20} />
+              </Link>
+            </div>
+
+            <div className={clsx(styles.heroButtons, styles.shieldBadges)}>
+              <Link to="https://github.com/ajnart/homarr">
+                <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+              </Link>
+              <Link to="https://discord.com/invite/aCsmEV5RgA">
+                <img src="https://img.shields.io/badge/Discord-5865F2?style=for-the-badge&logo=discord&logoColor=white" />
+              </Link>
+            </div>
+          </div>
+          <div className="col">
+            <ThemedDevicePreview />
           </div>
         </div>
       </div>
+
+      <Particles
+        url="/particles/particlesjs-config.json"
+        init={particlesInit}
+        canvasClassName={styles.particleCanvas}
+        style={{ position: 'absolute' }}
+      />
     </header>
   );
 }
@@ -39,11 +82,41 @@ export default function Home() {
       description="Homarr is a simple and lightweight homepage for your server, that helps you easily access all of your services in one place. It integrates with the services you use to display information on the homepage and helps your organize your services."
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        viewBox="0 0 1440 320"
+        className={styles.heroWave}
+      >
+        <path
+          fill="#fa5252"
+          fillOpacity="1"
+          d="M0,256L48,256C96,256,192,256,288,245.3C384,235,480,213,576,218.7C672,224,768,256,864,266.7C960,277,1056,267,1152,256C1248,245,1344,235,1392,229.3L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          data-darkreader-inline-fill=""
+        />
+      </svg>
+      <main>
         <HomepageShowcase />
+        <StyledDivider />
+        <HomepageFeatures />
+        <StyledDivider />
+        <HomepageUserReviews />
       </main>
     </Layout>
   );
 }
+
+const ThemedDevicePreview = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <img
+      className={styles.heroImage}
+      src={`/img/pictures/homarr-devices-preview/compressed/homarr-devices-2d-mockup-flat-shadow-${colorMode}-compressed.png`}
+      style={{
+        filter: 'drop-shadow(2px 4px 6px rgba(0,0,0,0.5))'
+      }}
+    />
+  );
+};
