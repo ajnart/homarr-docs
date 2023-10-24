42dbd6fc3b869197923b4a148dc61198511a74eb
ajnart <ajnart@pm.me>
2023-10-24 12:33:13 +0200

    Try fixing analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,6 +30,11 @@ const config = async function createConfig() {
     themes: ['@docusaurus/theme-mermaid'],
 
     scripts: [
+      {
+        src: "https://umami.ajnart.fr/script.js",
+        async: true,
+        dataWebsiteId: "2847e7dd-32a1-41f2-a6ed-2d9db17d71b9",
+      }
     ],
 
     presets: [
@@ -201,15 +206,6 @@ const config = async function createConfig() {
           },
         };
       },
-      "@dipakparmar/docusaurus-plugin-umami",
-      /** @type {import('@dipakparmar/docusaurus-plugin-umami').Options} */
-      ({
-        websiteID: "2847e7dd-32a1-41f2-a6ed-2d9db17d71b9", // Required
-        analyticsDomain: "https://umami.ajnart.fr", // Required
-        dataAutoTrack: true, // Optional
-        dataCache: true, // Optional
-        dataDomains: "https://homarr.dev", // comma separated list of domains, *Recommended*
-      }),
     ],
   };
 };
